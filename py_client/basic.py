c87b3260c4d7df3e116508eccb194241cdac1c6d
Imad Selka <imadselkawi133@gmail.com>
2024-02-22 14:44:12 +0100

    Add basic.py with API request

---
+++ py_client/basic.py
@@ -0,0 +1,12 @@
+import requests
+
+
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
+
+get_response = requests.get(endpoint,params={"abc":123} ,json={"query":"hello world"})
+
+# print(get_response.text)
+print(get_response.json())
+print(get_response.status_code)
