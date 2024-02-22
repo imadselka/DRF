ce82f01eeaab7399656084351643a3a009d419af
Imad Selka <imadselkawi133@gmail.com>
2024-02-22 23:58:14 +0100

    Update API request parameters

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint,params={"abc":123} ,json={"query":"hello world"})
+get_response = requests.get(endpoint,json={"product_id":123})
 
 # print(get_response.text)
 print(get_response.json())
