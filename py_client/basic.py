0c6a81b09750ad669ce2f897cb93778fc0506162
Imad Selka <imadselkawi133@gmail.com>
2024-02-23 23:32:11 +0100

    Update API endpoint and add JSON response parsing

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,21 +1,16 @@
 import requests
 
-endpoint = "http://localhost:8000/api/"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/ 
 
-headers = {
-    "Content-Type": "application/json"
-}
-
-get_response = requests.post(endpoint, json={"product_id": 123}, headers=headers)
-
-# Uncomment the following lines to print the response headers and text
+get_response = requests.post(endpoint, json={"title": "Abc123"}) # HTTP Request
 # print(get_response.headers)
-# print(get_response.text)
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
 
-if get_response.text:
-    try:
-        print(get_response.json())
-    except ValueError:
-        print("Response is not in JSON format")
-else:
-    print("Empty Response")
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Nototion ~ Python Dict
+print(get_response.json())
+# print(get_response.status_code)
